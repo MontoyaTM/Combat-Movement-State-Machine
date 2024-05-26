86847fc5a9dcb82a4f035425ada1c439cf186d42
Ricardo Montoya <montoya.ar94@gmail.com>
2024-05-26 12:40:37 -0400

    Added Game Controls
    Added mouse/keyboard & gamepad controls using unity's input system.

---
+++ Combat Movement State Machine/Assets/Scripts/StateMachine/Player/PlayerStateMachine.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStateMachine : StateMachine
+{
+    private void Start()
+    {
+        
+        SwitchState(new PlayerTestState(this));
+    }
+
+
+}
